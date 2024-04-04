237626e132a02307fd3edf1423b2a99a2080b51b
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2024-04-04 23:15:34 +0300

    mvpStart

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 "usestrict"
 
-const search = document.querySelector('.search');
-const icon = document.querySelector('.icon');
-const clear = document.querySelector('.clear');
-const input = document.getElementById('mySearch');
+// const search = document.querySelector('.search');
+// const icon = document.querySelector('.icon');
+// const clear = document.querySelector('.clear');
+// const input = document.getElementById('mySearch');
 
-icon.onclick = () => {
-    search.classList.toggle('active');
-}
+// icon.onclick = () => {
+//     search.classList.toggle('active');
+// }
+
+// clear.onclick = () => {
+//     input.value = "";
+// }
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "40442533-4b6791bab363289733298af78";
